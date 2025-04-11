a113a447a636a8369c68f007177afc39fa9a896a
Adi Patel <167858144+Shivaverma9638@users.noreply.github.com>
2025-04-10 22:55:59 -0700

    Update settings.py

--- adiprofile/settings.py
+++ adiprofile/settings.py
@@ -116,10 +116,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-# STATIC_URL = 'static/'
+STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-STATIC_URL = 'staticfiles/bands_ratings'
+# STATIC_URL = 'staticfiles/bands_ratings'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
